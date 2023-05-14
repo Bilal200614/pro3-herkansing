77b52e957a5d74ca90419cd117d6d36b0c26aee6
Bilal200614 <9019098@student.zadkine.nl>
2023-05-14 22:48:50 +0200

    first commit

---
+++ bilal.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Over mij</title>
+    <?php include "nav.php" ?>
+    <link rel="stylesheet" href="style.css">
+   
+
+    <body>
+	<div class="achtergrondfoto1">
+    <main>
+		<section>
+			<div class="text-box1">
+            <H1>Over ons</H1>
+            <br><br><br><br><br><br>
+            <p class="p1">
+Bilal Abbou is de gepassioneerde oprichter en ontwerper van BARA, een kledingbedrijf dat zich specialiseert in stijlvolle en eigentijdse pakken. Bilal heeft een oog voor detail en is altijd gefascineerd geweest door mode en design. Geboren en getogen in Marokko, heeft hij zijn culturele erfgoed gebruikt als inspiratiebron voor zijn creaties. <br> <br>
+
+BARA is ontstaan uit Bilal's liefde voor pakken en zijn verlangen om betaalbare, maar hoogwaardige maatpakken te creëren voor mannen. Het bedrijf is opgericht met als doel om de stijl van de moderne man te verbeteren en om een ongeëvenaarde pasvorm en kwaliteit te bieden.<br><br>
+
+Als oprichter en ontwerper is Bilal nauw betrokken bij elk aspect van het bedrijf, van de ontwerpfase tot de productie en verkoop. Hij heeft een getalenteerd team van ambachtslieden en ontwerpers samengesteld om ervoor te zorgen dat elk stuk van BARA perfect past en er fantastisch uitziet.
+<br><br>
+Met een groeiende reputatie en een snel groeiende klantenkring, blijft Bilal zich richten op het uitbreiden van het merk BARA en het creëren van nieuwe, innovatieve ontwerpen die mannen in staat stellen om er op hun best uit te zien.</p>
+        </div>
+		</section>
+         
+	</main>
+    </div>
+	
+	<?php include 'footer.php'; ?>
+</body>
+	
+</head>
+
+<link rel="website icon" type="png" href="img/Logo.png">
+
+</html>
---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bara Fashion</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<link rel="website icon" type="png" href="img/Logo.png">
+<body>
+	<?php include "nav.php" ?>
+	<div class="achtergrondfoto"></div>
+	<main>
+		<section>
+			<div class="text-box">
+            <h1>BARA Fashion</h1>
+            <p> Welkom bij onze  website.</p>
+            <a href="bilal.php" class="hero-btn">Visit us to know more</a>
+        </div>
+		</section>
+         
+	</main>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
